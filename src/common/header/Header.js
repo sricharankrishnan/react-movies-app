9dcd82909f96f3410326e9754c2d27cb2917bd61
sricharankrishnan <sricharan.krishnan@gmail.com>
2021-02-10 08:29:59 +0530

    Added Upcoming Movies heading on home page & added local movie data file

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -35,17 +35,28 @@ class Header extends Component {
       modalIsOpen: false,
       value: 0,
       usernameRequired: "dispNone",
+      passwordRequired: "dispNone",
+      firstnameRequired: "dispNone",
+      lastnameRequired: "dispNone",
+      emailRequired: "dispNone",
+      regPasswordRequired: "dispNone",
+      contactRequired: "dispNone",
       username: "",
-      password: ""
+      password: "",
+      firstname: "",
+      lastname: "",
+      email: "",
+      regPassword: "",
+      contact: ""
     };
   };
 
   openModalHandler() {
-    this.setState({modalIsOpen: true});
+    this.setState({modalIsOpen: true, value: 0});
   };
 
   closeModalHandler() {
-    this.setState({modalIsOpen: false});
+    this.setState({modalIsOpen: false, usernameRequired: "dispNone", passwordRequired: "dispNone"});
   };
 
   tabChangeHandler(event, toShowTabIndex) {
@@ -55,6 +66,19 @@ class Header extends Component {
   loginClickHandler(event) {
     let {username} = this.state;
     (username === "") ? this.setState({usernameRequired: "dispBlock"}) : this.setState({usernameRequired: "dispNone"});
+
+    let {password} = this.state;
+    (password === "") ? this.setState({passwordRequired: "dispBlock"}) : this.setState({passwordRequired: "dispNone"});
+  };
+
+  registerClickHandler(event) {
+    let {firstname, lastname, email, regPassword, contact} = this.state;
+    
+    (firstname === "") ? this.setState({firstnameRequired: "dispBlock"}) : this.setState({firstnameRequired: "dispNone"});
+    (lastname === "") ? this.setState({lastnameRequired: "dispBlock"}) : this.setState({lastnameRequired: "dispNone"});
+    (email === "") ? this.setState({emailRequired: "dispBlock"}) : this.setState({emailRequired: "dispNone"});
+    (regPassword === "") ? this.setState({regPasswordRequired: "dispBlock"}) : this.setState({regPasswordRequired: "dispNone"});
+    (contact === "") ? this.setState({contactRequired: "dispBlock"}) : this.setState({contactRequired: "dispNone"});
   };
 
   inputChangeHandler(event) {
@@ -108,10 +132,52 @@ class Header extends Component {
               <FormControl required centered="true">
                 <InputLabel htmlFor="password">Password</InputLabel>
                 <Input name="password" id="password" type="password" onChange={this.inputChangeHandler.bind(this)}/>
-                <FormHelpertext>Required</FormHelpertext>
+                <FormHelpertext className={this.state.passwordRequired}><span className="red">Required</span></FormHelpertext>
               </FormControl>
               <br/><br/>
               <Button variant="contained" color="primary" onClick={this.loginClickHandler.bind(this)}>Login</Button>
+            </TabContainer>
+          }
+          {
+            (this.state.value === 1) &&
+
+            <TabContainer>
+              <FormControl required centered="true">
+                <InputLabel htmlFor="firstname">First Name</InputLabel>
+                <Input name="firstname" id="firstname" type="text" firstname={this.state.firstname} onChange={this.inputChangeHandler.bind(this)}/>
+                <FormHelpertext className={this.state.firstnameRequired}><span className="red">Required</span></FormHelpertext>
+              </FormControl>
+              <br/>
+
+              <FormControl required centered="true">
+                <InputLabel htmlFor="lastname">Last Name</InputLabel>
+                <Input name="lastname" id="lastname" type="text" lastname={this.state.lastname} onChange={this.inputChangeHandler.bind(this)}/>
+                <FormHelpertext className={this.state.lastnameRequired}><span className="red">Required</span></FormHelpertext>
+              </FormControl>
+              <br/>
+
+              <FormControl required centered="true">
+                <InputLabel htmlFor="email">Email Name</InputLabel>
+                <Input name="email" id="email" type="email" email={this.state.email} onChange={this.inputChangeHandler.bind(this)}/>
+                <FormHelpertext className={this.state.emailRequired}><span className="red">Required</span></FormHelpertext>
+              </FormControl>
+              <br/>
+
+              <FormControl required centered="true">
+                <InputLabel htmlFor="regPassword">Password</InputLabel>
+                <Input name="regPassword" id="regPassword" type="password" email={this.state.regPassword} onChange={this.inputChangeHandler.bind(this)}/>
+                <FormHelpertext className={this.state.regPasswordRequired}><span className="red">Required</span></FormHelpertext>
+              </FormControl>
+              <br/>
+
+              <FormControl required centered="true">
+                <InputLabel htmlFor="contact">{`Contact Number`}</InputLabel>
+                <Input name="contact" id="contact" type="number" email={this.state.contact} onChange={this.inputChangeHandler.bind(this)}/>
+                <FormHelpertext className={this.state.contactRequired}><span className="red">Required</span></FormHelpertext>
+              </FormControl>
+              <br/>
+
+              <Button variant="contained" color="primary" onClick={this.registerClickHandler.bind(this)}>Register</Button>
             </TabContainer>
           }
         </Modal>
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,16 +1,41 @@
 import React, {Component} from "react";
-import "./Home.css";
+import { withStyles } from '@material-ui/core/styles';
+import moviesData from '../../assets/movieData';
 
 /* project imports */
 import Header from "../../common/header/Header.js";
+import "./Home.css";
+
+const styles = theme => ({
+   root: {
+       flexGrow: 1,
+       backgroundColor: theme.palette.background.paper
+   },
+   upcomingMoviesHeading: {
+       textAlign: 'center',
+       background: '#ff9999',
+       padding: '8px',
+       fontSize: '1rem',
+       fontFamily: "monospace"
+   },
+   gridListUpcomingMovies: {
+       flexWrap: 'nowrap',
+       transform: 'translateZ(0)',
+       width: '100%'
+   }
+});
 
 class Home extends Component {
   render() {
+    const {classes} = this.props;
     return (
       <React.Fragment>
         <Header/>
+        <div className={classes.upcomingMoviesHeading}>
+          <span>Upcoming Movies</span>
+        </div>
       </React.Fragment>
     );
   };
 }
-export default Home;
+export default withStyles(styles)(Home);
